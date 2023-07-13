3a3b2c2ac07beb17009e41624cc6e8bbfc910e04
riet <adamriet@gmail.com>
2023-07-13 15:33:40 -0600

    updated about me page

--- adam-r-portfolio/src/components/About.js
+++ adam-r-portfolio/src/components/About.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import myImage from '../styles/portfolio.jpg'; 
 import '../styles/styles.css';
 
 function About() {
     return (
-        <div className="container">
+        <div className="abcontainer">
+            <h2>About Me</h2> 
+            <img src={myImage} alt="About me" /> 
+            
             <p>With over a decade of experience in sales, I have developed a deep understanding of the importance of effective communication, relationship-building, and creative problem-solving. As a lifelong technology enthusiast, I am now channeling my passion into learning programming and expanding my skill set in the ever-evolving world of technology.</p>
 
             <p>Driven by my innate curiosity, I am continually exploring new ways to innovate and apply my expertise to real-world challenges. In my spare time, I enjoy immersing myself in the gaming world, where I sharpen my strategic thinking and problem-solving skills. Outside of work and gaming, I cherish the moments spent with friends, sharing experiences and creating lasting memories.</p>
