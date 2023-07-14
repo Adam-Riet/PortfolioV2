075a55941a03ffe737921b31ebd8fb4bb5a667a1
riet <adamriet@gmail.com>
2023-07-14 17:08:08 -0600

    added more projects, new styling for hover effects

--- adam-r-portfolio/src/components/Project.js
+++ adam-r-portfolio/src/components/Project.js
@@ -4,6 +4,10 @@ import githubLogo from '../assets/github-mark.svg';
 import sleepshop from '../assets/sleepshop.jpg';
 import socialmedia from '../assets/socialmedia.jpg';
 import notetaker from '../assets/notetaker.jpg';
+import blog from '../assets/blog.jpg';
+import ecommerce from '../assets/ecommerce.jpg';
+import readme from '../assets/readme.png';
+import dayplanner from '../assets/dayplanner.jpg';
 
 function Project() {
     return (
@@ -26,6 +30,28 @@ function Project() {
                         <img src={githubLogo} alt="GitHub Logo" className="github-logo" />
                     </a>
                     <a href="https://github.com/Adam-Riet/Note-Taker" target="_blank" rel="noopener noreferrer" className="project-title">Note Taker</a>
+                </div>
+                <div className="grid-item" style={{ backgroundImage: `url(${blog})` }}>
+                    <a href="https://techblog-2023-e186fd1c7525.herokuapp.com/" target="_blank" rel="noopener noreferrer" className="github-link">
+                        <img src={githubLogo} alt="GitHub Logo" className="github-logo" />
+                    </a>
+                    <a href="https://github.com/Adam-Riet/TechBlog" target="_blank" rel="noopener noreferrer" className="project-title2">Tech Blog</a>
+                </div>
+                <div className="grid-item" style={{ backgroundImage: `url(${ecommerce})` }}>
+                    <a href="https://github.com/Adam-Riet/Social-Network-API" target="_blank" rel="noopener noreferrer" className="github-link">
+                        <img src={githubLogo} alt="GitHub Logo" className="github-logo" />
+                    </a>
+                </div>
+                <div className="grid-item" style={{ backgroundImage: `url(${readme})` }}>
+                    <a href="https://github.com/Adam-Riet/README.generator" target="_blank" rel="noopener noreferrer" className="github-link">
+                        <img src={githubLogo} alt="GitHub Logo" className="github-logo" />
+                    </a>
+                </div>
+                <div className="grid-item" style={{ backgroundImage: `url(${dayplanner})` }}>
+                    <a href="https://adam-riet.github.io/Day-Planner/" target="_blank" rel="noopener noreferrer" className="github-link">
+                        <img src={githubLogo} alt="GitHub Logo" className="github-logo" />
+                    </a>
+                    <a href="https://github.com/Adam-Riet/Day-Planner" target="_blank" rel="noopener noreferrer" className="project-title">Tech Blog</a>
                 </div>
 
             </div>
