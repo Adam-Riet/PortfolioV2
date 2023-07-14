b6fbe4b3468eab30e2988104b5838da670986a2f
riet <adamriet@gmail.com>
2023-07-14 12:46:08 -0600

    added resume page. downloadable resume

---
+++ adam-r-portfolio/src/components/Resume.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import '../styles/styles.css';
+import resume from '../styles/resume.pdf';
+
+function Resume() {
+    return (
+        <div className="resumecontainer">
+            <h2>Resume</h2>
+            <a href={resume} download>Download my resume</a>
+            <h3>Programming Proficiencies:</h3>
+            <ul>
+                <li>JavaScript</li>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>React</li>
+                <li>Node</li>
+                <li>Express</li>
+                <li>MySQL</li>
+                <li>MongoDB</li>
+
+               
+            </ul>
+        </div>
+    );
+}
+
+export default Resume;
