e91e9bb7c2626baf74c3673df11dec3b6fb2eb0c
riet <adamriet@gmail.com>
2023-07-14 11:07:32 -0600

    rearranged nav bar. changed main font

---
+++ adam-r-portfolio/src/components/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/styles.css';
+
+
+function Contact() {
+    return (
+        <div className="container">
+            <h2>Contact</h2>
+        </div>
+    );
+}
+
+export default Contact;
